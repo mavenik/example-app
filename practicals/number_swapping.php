5b86e185f426fde07331646f1aecd6a1a6081b38
Nikhil Lanjewar <nikhil@lanjewar.com>
2023-10-31 11:35:41 +0530

    Add number swapping code

---
+++ practicals/number_swapping.php
@@ -0,0 +1,14 @@
+<?php
+$a = 5;
+$b = 10;
+
+echo "Before swapping: a = $a, b = $b \n";
+
+// Swapping logic using a temporary variable
+$temp = $a;
+$a = $b;
+$b = $temp;
+
+echo "After swapping: a = $a, b = $b \n";
+?>
+
