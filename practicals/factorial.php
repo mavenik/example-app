20d253c68d9a7c7ea2efc8abb609b6a03b87b069
Nikhil Lanjewar <nikhil@lanjewar.com>
2023-11-01 08:16:13 +0530

    Make factorial code readable and maintainable

--- practicals/factorial.php
+++ practicals/factorial.php
@@ -1,17 +1,18 @@
 <?php
+// Function to calculate the factorial
 function factorial($n) {
-    if ($n === 0) {
+    if ($n === 0 || $n === 1) {
         return 1;
     } else {
         return $n * factorial($n - 1);
     }
 }
 
-// Define the number for which you want to calculate the factorial
+// Define the number for which you want to find the factorial
 $number = 5;
 $result = factorial($number);
 
 // Display the factorial
-echo "Factorial of $number is: " . $result . "\n";
+echo "Factorial of $number is: $result\n";
 ?>
 
