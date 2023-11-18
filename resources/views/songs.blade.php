f54ed2798aa525e5587ff02b7f5cfa83749b355c
Nikhil Lanjewar <nikhil@lanjewar.com>
2023-11-18 13:18:27 +0530

    Update songs view to display multiple songs

--- resources/views/songs.blade.php
+++ resources/views/songs.blade.php
@@ -8,10 +8,12 @@
 <th>Title</th>
 <th>Artist</th>
 </tr>
+@foreach ($songs as $song)
 <tr>
 <td>{{ $song->getTitle() }}</td>
 <td>{{ $song->getArtist() }}</td>
 </tr>
+@endforeach
 </table>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -27,9 +27,17 @@ Route::get('/veggies/{veggieName}', function (string $veggieName) {
 })->whereIn('veggieName', ['baigan', 'bhindi', 'aaloo', 'gobhi']);
 
 Route::get('/songs', function () {
-  $song = new Song();
-  $song->setTitle("With You");
-  $song->setArtist("A P Dhillon");
+  $song1 = new Song();
+  $song1->setTitle("Stan");
+  $song1->setArtist("Eminem");
 
-  return view('songs', [ 'song' => $song ]);
+  $song2 = new Song();
+  $song2->setTitle("Nothing Else Matters");
+  $song2->setArtist("Metallica");
+
+  $song3 = new Song();
+  $song3->setTitle("With You");
+  $song3->setArtist("A P Dhillon");
+
+  return view('songs', [ 'songs' => [ $song1, $song2, $song3 ] ]);
 });
