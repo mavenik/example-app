aac3af6670579d1712766cf8beca43cd51206653
Nikhil Lanjewar <nikhil@lanjewar.com>
2023-11-21 21:14:58 +0530

    Display records from the database

--- resources/views/songs.blade.php
+++ resources/views/songs.blade.php
@@ -10,8 +10,8 @@
 </tr>
 @foreach ($songs as $song)
 <tr>
-<td>{{ $song->getTitle() }}</td>
-<td>{{ $song->getArtist() }}</td>
+<td>{{ $song->title }}</td>
+<td>{{ $song->artist }}</td>
 </tr>
 @endforeach
 </table>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Practicals\Song;
+use App\Models\Song;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,17 +27,5 @@ Route::get('/veggies/{veggieName}', function (string $veggieName) {
 })->whereIn('veggieName', ['baigan', 'bhindi', 'aaloo', 'gobhi']);
 
 Route::get('/songs', function () {
-  $song1 = new Song();
-  $song1->setTitle("Stan");
-  $song1->setArtist("Eminem");
-
-  $song2 = new Song();
-  $song2->setTitle("Nothing Else Matters");
-  $song2->setArtist("Metallica");
-
-  $song3 = new Song();
-  $song3->setTitle("With You");
-  $song3->setArtist("A P Dhillon");
-
-  return view('songs', [ 'songs' => [ $song1, $song2, $song3 ] ]);
+  return view('songs', [ 'songs' => Song::all() ]);
 });
