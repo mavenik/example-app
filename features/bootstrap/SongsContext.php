0eabbc1f7d806006a9e7671078c5e954c461bac0
Nikhil Lanjewar <nikhil@lanjewar.com>
2023-11-24 11:40:41 +0530

    Update SongsContext with step definitions

--- features/bootstrap/SongsContext.php
+++ features/bootstrap/SongsContext.php
@@ -12,8 +12,29 @@ use Behat\MinkExtension\Context\MinkContext;
  */
 class SongsContext extends MinkContext implements Context, SnippetAcceptingContext
 {
-	public function __construct()
-	{
-	}
+  public function __construct()
+  {
+  }
+
+  public static function getAcceptedSnippetType()
+  {
+    return 'regex';
+  }
+
+  /**
+   * @Given I visit the static songs page
+   */
+  public function iVisitTheStaticSongsPage()
+  {
+    $this->visit('/songs_static');
+  }
+
+  /**
+   * @Then I should see :arg1 as the title
+   */
+  public function iShouldSeeAsTheTitle($arg1)
+  {
+  }
+
 }
 ?>
