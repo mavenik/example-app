f2ad66e8543b27d8212733e68073768a7bf4f962
Nikhil Lanjewar <nikhil@lanjewar.com>
2023-11-25 12:19:40 +0530

    Add Main layout component

--- resources/views/songs.blade.php
+++ resources/views/songs.blade.php
@@ -1,19 +1,2 @@
-<html>
-<title>Songs Page</title>
-<body>
-<h2>Song - Blade Template</h2>
-
-<table>
-<tr>
-<th>Title</th>
-<th>Artist</th>
-</tr>
-@foreach ($songs as $song)
-<tr>
-<td>{{ $song->title }}</td>
-<td>{{ $song->artist }}</td>
-</tr>
-@endforeach
-</table>
-</body>
-</html>
+<x-layouts.main>
+</x-layouts.main>
